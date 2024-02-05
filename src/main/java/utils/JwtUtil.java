bd0a3226b77efe430c75e8a0c7ef9f44b458e917
wangyaning <357208746@qq.com>
2024-02-05 22:43:11 +0800

    Initial Commit

---
+++ src/main/java/utils/JwtUtil.java
@@ -0,0 +1,139 @@
+package utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import constant.Const;
+import jakarta.annotation.Resource;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Getter
+public class JwtUtil {
+    //用于给Jwt令牌签名校验的秘钥
+    @Value("${spring.security.jwt.key}")
+    private String key;
+    //令牌的过期时间，以小时为单位
+    @Value("${spring.security.jwt.expire}")
+    private int expire;
+    //为用户生成Jwt令牌的冷却时间，防止刷接口频繁登录生成令牌，以秒为单位
+    @Value("${spring.security.jwt.limit.base}")
+    private int limit_base;
+    //用户如果继续恶意刷令牌，更严厉的封禁时间
+    @Value("${spring.security.jwt.limit.upgrade}")
+    private int limit_upgrade;
+    //判定用户在冷却时间内，继续恶意刷令牌的次数
+    @Value("${spring.security.jwt.limit.frequency}")
+    private int limit_frequency;
+
+    @Resource
+    StringRedisTemplate template;
+
+    @Resource
+    FlowUtil utils;
+
+
+    /**
+     * 根据配置快速计算过期时间
+     * @return 过期时间
+     */
+    public Date expireTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR, expire);
+        return calendar.getTime();
+    }
+
+    /**
+     * 频率检测，防止用户高频申请Jwt令牌，并且采用阶段封禁机制
+     * 如果已经提示无法登录的情况下用户还在刷，那么就封禁更长时间
+     * @param userId 用户ID
+     * @return 是否通过频率检测
+     */
+    public boolean frequencyCheck(int userId){
+        String key = Const.JWT_FREQUENCY + userId;
+        return utils.limitOnceUpgradeCheck(key, limit_frequency, limit_base, limit_upgrade);
+    }
+
+    /**
+     * 让指定Jwt令牌失效
+     * @param headerToken 请求头中携带的令牌
+     * @return 是否操作成功
+     */
+    public boolean invalidateJwt(String headerToken){
+        String token = this.convertToken(headerToken);
+        Algorithm algorithm = Algorithm.HMAC256(key);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+            DecodedJWT verify = jwtVerifier.verify(token);
+            return deleteToken(verify.getId(), verify.getExpiresAt());
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 将Token列入Redis黑名单中
+     * @param uuid 令牌ID
+     * @param time 过期时间
+     * @return 是否操作成功
+     */
+    public boolean deleteToken(String uuid, Date time){
+        if(this.isInvalidToken(uuid))
+            return false;
+        Date now = new Date();
+        long expire = Math.max(time.getTime() - now.getTime(), 0);
+        template.opsForValue().set(Const.JWT_BLACK_LIST + uuid, "", expire, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    /**
+     * 解析Jwt令牌
+     * @param headerToken 请求头中携带的令牌
+     * @return DecodedJWT
+     */
+    public DecodedJWT resolveJwt(String headerToken){
+        String token = this.convertToken(headerToken);
+        if(token == null) return null;
+        Algorithm algorithm = Algorithm.HMAC256(key);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+            DecodedJWT verify = jwtVerifier.verify(token);
+            if(this.isInvalidToken(verify.getId())) return null;
+            Map<String, Claim> claims = verify.getClaims();
+            return new Date().after(claims.get("exp").asDate()) ? null : verify;
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 校验并转换请求头中的Token令牌
+     * @param headerToken 请求头中的Token
+     * @return 转换后的令牌
+     */
+    public String convertToken(String headerToken){
+        if(headerToken == null || !headerToken.startsWith("Bearer "))
+            return null;
+        return headerToken.substring(7);
+    }
+
+    /**
+     * 验证Token是否被列入Redis黑名单
+     * @param uuid 令牌ID
+     * @return 是否操作成功
+     */
+    public boolean isInvalidToken(String uuid){
+        return Boolean.TRUE.equals(template.hasKey(Const.JWT_BLACK_LIST + uuid));
+    }
+}
