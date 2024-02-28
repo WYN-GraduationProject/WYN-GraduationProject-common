5fa1bc714c81291dbb2f9cf47c75f34122a5e892
wangyaning <357208746@qq.com>
2024-02-28 22:16:40 +0800

    fix(Redis): 已经修复Redis序列化问题

--- src/main/java/config/RedisConfiguration.java
+++ src/main/java/config/RedisConfiguration.java
@@ -1,9 +1,11 @@
 package config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import utils.RedisSerializerUtils;
@@ -16,20 +18,22 @@ public class RedisConfiguration {
      * 自定义一个RedisTemplate
      */
     @Bean
+    @SuppressWarnings("all")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // String类型 key序列器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // String类型 value序列器
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        // 使用FastJson2RedisSerializer来序列化和反序列化redis的value值
-        RedisSerializerUtils<Object> serializer = new RedisSerializerUtils<>(Object.class);
+        // Hash类型 key序列器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // Hash类型 value序列器
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        // 设置value的序列化规则和 key的序列化规则
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
+        // 把连接工厂放进去
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-        template.afterPropertiesSet();
-        return template;
+        return redisTemplate;
     }
 }
