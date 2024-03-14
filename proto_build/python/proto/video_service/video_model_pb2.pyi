138da953f0e1393b7b88fefc93038724798da7a6
wangyaning <357208746@qq.com>
2024-03-14 22:58:55 +0800

    perf(common,preprocessing): 修改文件目录，优化文件结构

--- proto_build/python/proto/video_service/video_model_pb2.pyi
+++ proto_build/python/proto/video_service/video_model_pb2.pyi
@@ -5,19 +5,21 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class VideoFrame(_message.Message):
-    __slots__ = ["data", "is_final", "fps"]
+    __slots__ = ["data", "is_final", "fps", "video_id"]
     DATA_FIELD_NUMBER: _ClassVar[int]
     IS_FINAL_FIELD_NUMBER: _ClassVar[int]
     FPS_FIELD_NUMBER: _ClassVar[int]
+    VIDEO_ID_FIELD_NUMBER: _ClassVar[int]
     data: bytes
     is_final: bool
     fps: float
-    def __init__(self, data: _Optional[bytes] = ..., is_final: bool = ..., fps: _Optional[float] = ...) -> None: ...
+    video_id: str
+    def __init__(self, data: _Optional[bytes] = ..., is_final: bool = ..., fps: _Optional[float] = ..., video_id: _Optional[str] = ...) -> None: ...
 
 class ProcessedVideoFrame(_message.Message):
-    __slots__ = ["data", "is_final"]
+    __slots__ = ["data", "video_path"]
     DATA_FIELD_NUMBER: _ClassVar[int]
-    IS_FINAL_FIELD_NUMBER: _ClassVar[int]
+    VIDEO_PATH_FIELD_NUMBER: _ClassVar[int]
     data: bytes
-    is_final: bool
-    def __init__(self, data: _Optional[bytes] = ..., is_final: bool = ...) -> None: ...
+    video_path: str
+    def __init__(self, data: _Optional[bytes] = ..., video_path: _Optional[str] = ...) -> None: ...
